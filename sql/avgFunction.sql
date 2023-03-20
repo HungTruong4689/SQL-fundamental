f2e1cf00d8ea40b10cc8e5d13aa880ea7b4a839c
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-20 20:01:35 +0200

    avg function

--- sql/avgFunction.sql
+++ sql/avgFunction.sql
@@ -3,3 +3,4 @@ Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
 select avg(population) from city where district = 'California';
+-- average function
